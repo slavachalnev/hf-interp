e9a270812be01f07b8a968cca77a1fc3487d3620
Slava Chalnev <slava.chalnev@gmail.com>
2023-06-24 13:42:44 +1000

    Add setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hf_interp',
+    version='0.1.0',
+    url='https://github.com/slavachalnev/hf-interp',
+    author='Sviatoslav Chalnev',
+    description='Hooked Transformer interpretability based on HuggingFace Transformers',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "torch",
+        "transformers",
+        "accelerate",
+        "einops",
+        "fancy_einsum",
+        "tqdm",
+    ],
+)
