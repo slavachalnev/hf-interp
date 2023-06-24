53e7c243f6f5b81ded4392356e68fa2530dd9cb0
Slava Chalnev <slava.chalnev@gmail.com>
2023-06-24 15:12:00 +1000

    Add post init checks

--- hf_interp/config.py
+++ hf_interp/config.py
@@ -1,6 +1,12 @@
 from typing import Optional, List
-
+import logging
+import random
+import numpy as np
+import torch
 from transformers import PretrainedConfig
+
+
+SUPPORTED_ACTIVATIONS = ["relu", "gelu", "silu", "gelu_new", "solu_ln", "gelu_fast"]
 
 
 class HookedTransformerConfig(PretrainedConfig):
@@ -92,3 +98,66 @@ class HookedTransformerConfig(PretrainedConfig):
         self.n_params = n_params
         self.use_hook_tokens = use_hook_tokens
         self.gated_mlp = gated_mlp
+
+        self.post_init()
+    
+
+    def post_init(self):
+        if self.n_heads == -1:
+            self.n_heads = self.d_model // self.d_head
+
+            if not self.d_model % (self.d_head) == 0:
+                logging.warning(
+                    f"d_model {self.d_model} is not divisible by d_head {self.d_head}. n_heads was inferred to be {self.n_heads}, rounding down the ratio."
+                )
+
+        if self.seed is not None:
+            self.set_seed_everywhere(self.seed)
+        if self.use_local_attn:
+            assert (
+                self.window_size is not None
+            ), "window_size must be specified for local attention"
+            assert (
+                self.attn_types is not None
+            ), "attn_types must be specified for local attention"
+        if not self.attn_only:
+            if self.d_mlp is None:
+                # For some reason everyone hard codes in this hyper-parameter!
+                self.d_mlp = self.d_model * 4
+            assert (
+                self.act_fn is not None
+            ), "act_fn must be specified for non-attn-only models"
+            assert (
+                self.act_fn in SUPPORTED_ACTIVATIONS
+            ), f"act_fn={self.act_fn} must be one of {SUPPORTED_ACTIVATIONS}"
+        if self.initializer_range < 0:
+            # Roughly copy the GPT-2 value, but proportional to sqrt(1/d_model)
+            self.initializer_range = 0.8 / np.sqrt(self.d_model)
+
+        if self.d_vocab_out == -1:
+            # d_vocab_out defaults to d_vocab, unless there's an algorithmic task
+            # If d_vocab is not set, it'll be inferred from tokenizer_name or from a tokenizer explicitly passed to HookedTransformer initialisation.
+            self.d_vocab_out = self.d_vocab
+
+        if self.positional_embedding_type == "rotary" and self.rotary_dim is None:
+            self.rotary_dim = self.d_head
+
+        # The number of parameters in attention layers (ignoring biases and layer norm). 4 because W_Q, W_K, W_V and W_O
+        self.n_params = self.n_layers * (
+            (self.d_model * self.d_head * self.n_heads * 4)
+        )
+        if not self.attn_only:
+            # Number of parameters in MLP layers (ignoring biases and layer norm). 2 because W_in and W_out
+            self.n_params += self.n_layers * self.d_model * self.d_mlp * 2
+
+    @classmethod
+    def from_dict(cls, config_dict):
+        # TODO: inherit from PretrainedConfig.from_dict
+        config = cls(**config_dict)
+        return config
+    
+    def set_seed_everywhere(self, seed: int):
+        torch.manual_seed(seed)
+        random.seed(seed)
+        np.random.seed(seed)
+
